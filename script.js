9ff3bf6bb802d4a288e08e19644bca9964bc1b56
Michal Szczepaniak <michal.szczepaniak@inspirelabs.pl>
2019-11-29 10:23:12 +0100

    update select inputs

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
-const check = () => {
+
+
+
+const checkk = () => {
     let getEl = [...document.querySelectorAll("input")];
     // console.log(getEl);
     let howManyInputs = 0;
 
     getEl.forEach(e => {
-        // console.log(e);
         if (e.checked) {
             e.checked = false;
-        } else if (e.checked === false) {
+        }
+        else if (e.checked === false) {
             e.checked = true;
         }
         if (e.value === '') {
@@ -18,5 +21,52 @@ const check = () => {
     console.log(howManyInputs);
 };
 
+
+
+
+const check = () => {
+    let getEl = [...document.querySelectorAll("input")];
+    // console.log(getEl);
+    let howManyInputs = 0;
+
+    getEl.forEach(e => {
+        console.log(e);
+        if (e.checked) {
+            e.checked = false;
+        }
+        if (e.checked === false) {
+            e.checked = true;
+        }
+        if (e.value === '') {
+            e.value = "przykładowy text";
+        }
+        howManyInputs++;
+    });
+    console.log(howManyInputs);
+};
+
+
+
 const btn = document.querySelector('#selectAllInputs');
-btn.addEventListener('click', check);
+btn.addEventListener('click', checkk);
+
+
+
+const checkkk = () => {
+    let getEl = [...document.querySelectorAll("input")];
+    let howManyInputs = 0;
+
+    getEl.forEach(e => {
+        if (e.checked) {
+            e.checked = false;
+        }
+        else if (e.checked === false) {
+            e.checked = true;
+        }
+        if (e.value === '') {
+            e.value = "33-222";
+        }
+        howManyInputs++;
+    });
+    console.log(howManyInputs);
+};
